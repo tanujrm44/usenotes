5487599ab8a87a837f27c22e75f1d8a64c8c5f4c
Tanuj <tanujrm44@gmail.com>
2024-02-04 14:34:14 +0530

    edit note 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,28 @@
+import { connectToDB } from '@/db/db';
 import { redirect } from 'next/navigation';
+import Notes from '@/models/notesModel'
 import React from 'react'
+import Link from 'next/link';
 
-export default function page() {
-
-  interface TitleTypes {
-    id: string;
-    title: string;
-  }
-
-  const titles: TitleTypes[] = [
-    { id: "1", title: 'Title 1' },
-    { id: "2", title: 'Title 2' },
-    { id: "3", title: 'Title 3' },
-  ];
+export default async function page() {
+  connectToDB()
+  const notes = await Notes.find()
 
   return (
     <>
       <h2 className="text-2xl font-bold mb-4">Notes</h2>
       <ul>
-        {titles.map((item) => (
-          <li key={item.id} className="flex items-center justify-between bg-gray-100 shadow-md rounded-md p-4 mb-4 cursor-pointer md:w-1/2">
+        {notes?.map((item) => (
+          <li key={item._id} className="flex items-center justify-between bg-gray-100 shadow-md rounded-md p-4 mb-4 cursor-pointer md:w-1/2">
             <span className="text-lg">{item.title}</span>
             <div className="space-x-2">
-              <button
-                className="bg-blue-500 text-white px-3 py-1 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-              //onClick={() => redirect(`/note/${item.id}/edit`)}
-              >
-                Edit
-              </button>
+              <Link href={`/note/${item._id}/edit`}>
+                <button
+                  className="bg-blue-500 text-white px-3 py-1 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                >
+                  Edit
+                </button>
+              </Link>
               <button
                 className="bg-red-500 text-white px-3 py-1 rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:border-red-300"
               >
